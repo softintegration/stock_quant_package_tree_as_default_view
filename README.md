1d065545bfe54698fcaaaa278d20edaa43b2d664
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-12-04 14:55:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Make the tree view of the packages as the default view
+------------------------------------------------------
+This module make the tree view of package as the default one.
+
+
+
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+    'name': 'Make the tree view of the packages as the default view',
+    'author': 'Soft-integration',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1',
+    'category': 'Stock',
+    'demo': [],
+    'depends': ['stock'],
+    'data': [
+        'views/stock_quant_views.xml',
+    ],
+    'license': 'LGPL-3',
+}
